551e0f2840e802e3dc6e51089c748cb89763fbdb
Jeremy Epstein <jazepstein@gmail.com>
2015-04-20 10:12:50 +1000

    fix up admin.py formatting a bit

--- project/foo/admin.py
+++ project/foo/admin.py
@@ -10,7 +10,8 @@ from library.prefix_file_utcnow import prefix_file_utcnow
 
 class ThingyView(ProtectedModelView):
     column_list = ('image',)
-    form_excluded_columns = ('image_storage_type', 'image_storage_bucket_name')
+    form_excluded_columns = ('image_storage_type',
+                             'image_storage_bucket_name')
 
     form_overrides = dict(
         image=S3ImageUploadField)
@@ -27,15 +28,20 @@ class ThingyView(ProtectedModelView):
 
     def scaffold_form(self):
         form_class = super(ThingyView, self).scaffold_form()
-        static_root_parent = path.abspath(path.join(app.config['PROJECT_ROOT'], '..'))
+        static_root_parent = path.abspath(
+            path.join(app.config['PROJECT_ROOT'], '..'))
 
         if app.config['USE_S3']:
             form_class.image.kwargs['storage_type'] = 's3'
 
-        form_class.image.kwargs['bucket_name'] = app.config['S3_BUCKET_NAME']
-        form_class.image.kwargs['access_key_id'] = app.config['AWS_ACCESS_KEY_ID']
-        form_class.image.kwargs['access_key_secret'] = app.config['AWS_SECRET_ACCESS_KEY']
-        form_class.image.kwargs['static_root_parent'] = static_root_parent
+        form_class.image.kwargs['bucket_name'] = \
+            app.config['S3_BUCKET_NAME']
+        form_class.image.kwargs['access_key_id'] = \
+            app.config['AWS_ACCESS_KEY_ID']
+        form_class.image.kwargs['access_key_secret'] = \
+            app.config['AWS_SECRET_ACCESS_KEY']
+        form_class.image.kwargs['static_root_parent'] = \
+            static_root_parent
 
         return form_class
 
